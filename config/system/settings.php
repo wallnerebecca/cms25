bd5e0b7e8ecef0c6bdcc257c721646be30d42a68
Becii <wallnerebecca@gmail.com>
2026-01-11 20:36:08 +0100

    save

---
+++ config/system/settings.php
@@ -0,0 +1,169 @@
+<?php
+return [
+    'BE' => [
+        'debug' => true,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$akowQVpOYi9VRVRSdzZqcQ$MBYGYh0e1kuyr3I8Emu9NIBHNdU7lh/eXjQnnj4b88w',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'mevjnn_db1',
+                'defaultTableOptions' => [
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                ],
+                'driver' => 'mysqli',
+                'host' => '127.0.0.1',
+                'password' => 'g6#B-{kwKS]2',
+                'port' => 3306,
+                'user' => 'mevjnn_1',
+            ],
+        ],
+    ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'bootstrap_package' => [
+            'disableCssProcessing' => '0',
+            'disableGoogleFontCaching' => '0',
+            'disablePageTsBackendLayouts' => '0',
+            'disablePageTsContentElements' => '0',
+            'disablePageTsRTE' => '0',
+            'disablePageTsTCADefaults' => '0',
+            'disablePageTsTCEFORM' => '0',
+            'disablePageTsTCEMAIN' => '0',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'mask' => [
+            'backend' => 'EXT:kwm2025/Resources/Private/Mask/Backend/Templates',
+            'backend_layouts_folder' => '',
+            'backendlayout_pids' => '0',
+            'content' => 'EXT:kwm2025/Resources/Private/Mask/Frontend/Templates',
+            'content_elements_folder' => '',
+            'json' => 'EXT:kwm2025/Configuration/Mask/mask.json',
+            'layouts' => 'EXT:kwm2025/Resources/Private/Mask/Frontend/Layouts',
+            'layouts_backend' => 'EXT:kwm2025/Resources/Private/Mask/Backend/Layouts',
+            'loader_identifier' => 'json',
+            'override_shared_fields' => '0',
+            'partials' => 'EXT:kwm2025/Resources/Private/Mask/Frontend/Partials',
+            'partials_backend' => 'EXT:kwm2025/Resources/Private/Mask/Backend/Partials',
+            'preview' => 'EXT:kwm2025/Resources/Public/Mask/',
+        ],
+        'news' => [
+            'advancedMediaPreview' => '1',
+            'archiveDate' => 'date',
+            'categoryBeGroupTceFormsRestriction' => '0',
+            'categoryRestriction' => '',
+            'contentElementRelation' => '1',
+            'dateTimeNotRequired' => '0',
+            'hidePageTreeForAdministrationModule' => '0',
+            'manualSorting' => '0',
+            'pageTreePluginPreview' => '1',
+            'prependAtCopy' => '1',
+            'resourceFolderImporter' => '/news_import',
+            'rteForTeaser' => '0',
+            'showAdministrationModule' => '1',
+            'slugBehaviour' => 'unique',
+            'storageUidImporter' => '1',
+            'tagPid' => '1',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => true,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'defaultMailFromAddress' => 's2410456031@students.fh-hagenberg.at',
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/sbin/sendmail -t -i',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '*',
+        'displayErrors' => 1,
+        'encryptionKey' => 'a83d978a612372947e6edf296f4bb13e5abb799f153309e7002b9f3e53def6b5215bae3da715bb6d288f65401d95c777',
+        'exceptionalErrors' => 12290,
+        'features' => [
+            'extbase.consistentDateTimeHandling' => true,
+            'frontend.cache.autoTagging' => true,
+            'security.system.enforceAllowedFileExtensions' => true,
+        ],
+        'sitename' => 'CMS25',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
